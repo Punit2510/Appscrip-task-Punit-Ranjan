6acbc28f6ab39fbe017e899716db526fcd5b1d12
Daksh Arya <arya.daksh.official@gmail.com>
2026-03-28 16:50:46 +0530

    chore: set up design tokens, CSS reset, and Next.js image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
+        port: '',
+        pathname: '/**',
+      },
+      // In case they have mock responses with other domains, better safe than sorry
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
 
 export default nextConfig;
